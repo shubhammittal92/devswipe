ad8fae5e4b5058c95c93268602cb2e1c2446c1b8
Shubham mittal <shubmittal.92@gmail.com>
2025-02-22 03:13:59 +0530

    deplyment url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
 
 
-export const BASE_URL= location.hostname === "localhost" ? "https://devtinder-2-i0yj.onrender.com/" : "/api";
+export const BASE_URL= location.hostname === "localhost" ? "https://devtinder-3-a7nl.onrender.com/" : "/api";
