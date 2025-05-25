87c680444d5f176561db0891e6f80356e6781050
Shubham mittal <shubmittal.92@gmail.com>
2025-05-25 22:15:05 +0530

    user card updated

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,11 +1,10 @@
-
 import axios from "axios";
 import { BASE_URL } from "../utils/constants";
 import { useDispatch } from "react-redux";
 import { removeUserFromFeed } from "../utils/feedSlice";
 
 const UserCard = ({ user }) => {
-  const { _id, firstName, lastName, photoUrl, age, gender, about } = user;
+  const { _id, firstName, lastName, photoUrl, age, gender, about, skills } = user;
   const dispatch = useDispatch();
 
   const handleSendRequest = async (status, userId) => {
@@ -22,12 +21,30 @@ const UserCard = ({ user }) => {
   return (
     <div className="card bg-base-300 w-96 shadow-xl">
       <figure>
-        <img src={user.photoUrl} alt="photo" />
+        <img src={photoUrl} alt="photo" />
       </figure>
       <div className="card-body">
         <h2 className="card-title">{firstName + " " + lastName}</h2>
         {age && gender && <p>{age + ", " + gender}</p>}
         <p>{about}</p>
+        
+        {/* Skills section */}
+        {skills && skills.length > 0 && (
+          <div className="my-2">
+            <h3 className="font-semibold mb-2">Skills:</h3>
+            <div className="flex flex-wrap gap-2">
+              {skills.map((skill, index) => (
+                <span 
+                  key={index} 
+                  className="badge badge-outline rounded-full py-2 px-4"
+                >
+                  {skill}
+                </span>
+              ))}
+            </div>
+          </div>
+        )}
+        
         <div className="card-actions justify-center my-4">
           <button
             className="btn btn-primary"
@@ -46,4 +63,5 @@ const UserCard = ({ user }) => {
     </div>
   );
 };
+
 export default UserCard;
