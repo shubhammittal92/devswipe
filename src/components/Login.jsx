a97888c8f699c2e654d301c9e1acf73be735a52c
Shubham mittal <shubmittal.92@gmail.com>
2025-05-27 01:12:21 +0530

    sign up issue updated

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -40,7 +40,7 @@ const Login = () => {
         { withCredentials: true }
       );
       dispatch(addUser(res.data.data));
-      return navigate("/profile/edit");
+      return navigate("/");
     } catch (err) {
       setError(err?.response?.data || "Something went wrong");
     }
